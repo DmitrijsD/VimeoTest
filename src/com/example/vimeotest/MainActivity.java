c54817cc97eb0f6b3a623a69cfc18d7bca3fa683
Nelson Perez <bilthon@gmail.com>
2013-08-28 03:20:20 -0300

    Removed unused files

--- src/com/example/vimeotest/MainActivity.java
+++ src/com/example/vimeotest/MainActivity.java
@@ -7,39 +7,30 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebSettings.PluginState;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 	private final String TAG = "MainActivity";
-	private WebView mWebView;
+	private HTML5WebView mWebView;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		mWebView = (WebView) findViewById(R.id.WebView);
+		mWebView = new HTML5WebView(this);
+		
 		//Auto playing vimeo videos in Android webview
 		mWebView.getSettings().setJavaScriptEnabled(true);
+		mWebView.getSettings().setAllowFileAccess(true);
 		mWebView.getSettings().setAppCacheEnabled(true);
 		mWebView.getSettings().setDomStorageEnabled(true);
-		mWebView.getSettings().setPluginState(PluginState.ON);
+		mWebView.getSettings().setPluginState(PluginState.OFF);
 		mWebView.getSettings().setAllowFileAccess(true);
 		
-		mWebView.setWebChromeClient(new MyWebChromeClient());
-		mWebView.setWebViewClient(new WebViewClient(){
-	        @Override
-	        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-	            return false;
-	        }
-	        @Override
-	        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-	        	Log.e(TAG,"Error. Msg: "+description);
-	          }
-	    });
 //		mWebView.loadUrl("http://player.vimeo.com/video/24577973?player_id=player&autoplay=1&title=0&byline=0&portrait=0&api=1&maxheight=480&maxwidth=800");
-//		mWebView.loadUrl("file:///android_asset/simple.html");
+//		mWebView.loadUrl("http://player.vimeo.com/api/examples/simple");
 		mWebView.loadUrl("file:///android_asset/1_0.html");
-//		mWebView.loadUrl("http://192.168.1.4:9090/1_0.html");
+//		mWebView.loadUrl("http://192.168.1.4:9090/playground.html");
+//		mWebView.loadUrl("http://ua.brad.is");
+		setContentView(mWebView.getLayout());
 	}
 
 	@Override
@@ -58,6 +49,4 @@ public class MainActivity extends Activity {
 		}
 		return super.onMenuItemSelected(featureId, item);
 	}
-
-	
 }
